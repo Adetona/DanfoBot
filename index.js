b8251546b32d8f52065de9dda438fd257648c780
Abiodun <adetonaabiodun12@gmail.com>
2019-03-22 13:04:55 +0100

    Added express

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-//Load HTTP module
-const http = require("http");
-const hostname = '127.0.0.1';
-const port = 3000;
+'use strict';
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+// Imports dependencies and set up http server
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const  express = require('express');
+
+const app = express();
+
+
+
+  app.get('/', function(req, res){
+      res.send('Hi I am a messenger bot');
+    });
+
+
+app.listen(process.env.PORT || 5000, () => console.log('DanfoBot is listening'));
