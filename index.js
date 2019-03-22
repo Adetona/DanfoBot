b29759eb6f5dcb011425b6bf8d1c7585857928d0
Abiodun <adetonaabiodun12@gmail.com>
2019-03-22 13:19:46 +0100

    Added index.html

--- index.js
+++ index.js
@@ -4,14 +4,19 @@
 
 
 const  express = require('express');
-
 const app = express();
+const path = require('path');
 
 
 
-  app.get('/', function(req, res){
+/*  app.get('/', function(req, res){
       res.send('Hi I am a messenger bot');
-    });
+    });*/
+
+    //assuming app is express Object.
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 
 app.listen(process.env.PORT || 5000, () => console.log('DanfoBot is listening'));
