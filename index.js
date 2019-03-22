ef2647b3c7a7d16657fdea842548d5999f83c84e
Abiodun <adetonaabiodun12@gmail.com>
2019-03-22 18:07:30 +0100

    Added danfomessage

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ app.get('/',function(req,res) {
 });
 
 
-app.listen(process.env.PORT || 5000, () => console.log('DanfoBot is listening'));
+app.listen(process.env.PORT || 5000, () => console.log('DanfoBot is listening at 5000'));
