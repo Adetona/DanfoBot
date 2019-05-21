b6884204c825e989bdf3f90a08ed99e76e1c58d5
Abiodun <adetonaabiodun12@gmail.com>
2019-05-21 06:33:43 +0100

    manifest

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.use(express.static('/'));
+app.use('/',express.static(__dirname));
+
+//app.use(express.static('public'));
 //app.use(express.static(__dirname + '/'));
 
 
